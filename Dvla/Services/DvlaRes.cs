1005cc1e7725d6f27d9aea1eeb97960776b151ea
Theo Arnold <theohgarnold@gmail.com>
2024-11-26 00:04:38 +0000

    Update DvlaRes.cs

--- Dvla/Services/DvlaRes.cs
+++ Dvla/Services/DvlaRes.cs
@@ -23,7 +23,7 @@ namespace Dvla.Services
         public string PrimaryColour { get; set; }
 
         [JsonPropertyName("motTests")]
-        public IEnumerable<MotTest> MotTests { get; set; }
+        public IReadOnlyCollection<MotTest> MotTests { get; set; }
     }
 
     public class MotTest
@@ -48,7 +48,7 @@ namespace Dvla.Services
         public string MotTestNumber { get; set; }
 
         [JsonPropertyName("rfrAndComments")]
-        public IEnumerable<RfrAndComment> RfrAndComments { get; set; }
+        public IReadOnlyCollection<RfrAndComment> RfrAndComments { get; set; }
     }
 
     public class RfrAndComment
